b04c0f4ed61148fc316a978f3d2db44e7cf2e345
Danielacarranza <danielacarcam@gmail.com>
2021-11-26 21:36:31 -0600

    App Musica - 1902224

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+firebaseConfig : {
+    apiKey: "AIzaSyDdRaCn1rfNdT1573tP040YdapvngNWDsg",
+    authDomain: "musica-8e99a.firebaseapp.com",
+    databaseURL: "https://musica-8e99a-default-rtdb.firebaseio.com",
+    projectId: "musica-8e99a",
+    storageBucket: "musica-8e99a.appspot.com",
+    messagingSenderId: "509175662935",
+    appId: "1:509175662935:web:ebc6b110e995ea72541612"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
